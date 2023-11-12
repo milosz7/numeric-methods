d98bf73926cb5c8e4c91749b859ad0ec795b9687
Milosz B <miloszbargiel@gmail.com>
2023-11-12 15:19:50 +0100

    Improve numerical stability of ex3 algorithm

--- zadanie3.py
+++ zadanie3.py
@@ -17,14 +17,19 @@ A /= 12
 # choose a random vector
 v0 = np.random.random(A.shape[0]).reshape(A.shape[0], 1)
 v0 /= np.linalg.norm(v0)
+v_found = None
 for k in range(iterations):
     v_new = A @ v0
+    if v_found is not None:
+        v_new = v_new - v_found * (v_found.T @ v_new)
+
     v_new = v_new / np.linalg.norm(v_new) 
 
     if np.linalg.norm(np.abs(v_new - v0)) < precision:
         eigenval = (v_new.T @ A @ v_new)
         print(v_new, eigenval)
-        # matrix deflation
-        A = A - eigenval * (v_new @ v_new.T)
+        if v_found is not None:
+            break
+        v_found = v_new
 
     v0 = v_new
