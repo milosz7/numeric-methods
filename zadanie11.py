39362e0205759398f65ee33cb62b7d477ab0944a
Miłosz Bargieł <milosz7@MacBook-Pro-Miosz.local>
2023-12-19 11:18:34 +0100

    Zadanie11 algorithm efficiency improvements

--- zadanie11.py
+++ zadanie11.py
@@ -9,6 +9,8 @@ def find_limit(tolerance=1e-7):
 
 
 def measure_error(approx_old, approx_new, tolerance=1e-7, epsilon=1e-5):
+    if approx_old is None:
+        return False
     return abs(approx_new - approx_old) / (abs(approx_old) + epsilon) < tolerance
 
 
@@ -17,17 +19,15 @@ def f_x(x):
 
 
 def calculate_integral(x_start, x_end):
-    approx_old = 1e10
+    approx_old = None
     n_elements = 2
-    constant_factor = (f_x(x_start) + f_x(x_end)) / 2.0
+    calculated_vals = (f_x(x_start) + f_x(x_end)) / 2.0
 
     while True:
-        splits = np.linspace(x_start, x_end, n_elements)
-        approx_new = constant_factor
-        jump = abs(splits[0] - splits[1])
-        for i in range(1, len(splits) - 1):
-            approx_new += f_x(splits[i])
-        approx_new *= jump
+        splits, jump = np.linspace(x_start, x_end, n_elements, retstep=True)
+        for value in splits[1::2]:
+            calculated_vals += f_x(value)
+        approx_new = calculated_vals * jump
         if measure_error(approx_old, approx_new):
             print("Integration result is:", approx_new)
             break
